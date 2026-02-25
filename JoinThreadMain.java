7ec5d79c34a93063dd197a08e525e53d4bf7bfc6
Anurag Kapoor <anuragkapoor@Anurags-MacBook-Pro.local>
2026-02-25 23:05:47 +0530

    feat: thread join example

---
+++ JoinThreadMain.java
@@ -0,0 +1,21 @@
+public class JoinThreadMain {
+    public static void main(String[] args) {
+        System.out.println("Main function starts");
+        ResumeSuspendResource resumeSuspendResource = new ResumeSuspendResource();
+
+        Thread thread1 = new Thread(() -> {
+            resumeSuspendResource.produce();
+            System.out.println("Thread 1 completed");
+        });
+
+        thread1.start();
+
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Main function ends");
+    }
+}
